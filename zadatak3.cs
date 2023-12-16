f8c178d8e0d124573d2455d743aea5da4c01575b
gogi150 <153612601+gogi150@users.noreply.github.com>
2023-12-16 22:41:53 +0100

    Add files via upload

---
+++ zadatak3.cs
@@ -0,0 +1,27 @@
+static void Main(string[] args)
+    {
+        Console.WriteLine("Unesite vrijednost stranice x:");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Unesite vrijednost stranice y:");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Unesite vrijednost stranice z:");
+        int z = int.Parse(Console.ReadLine());
+
+        if (x > 0 && y > 0 && z > 0)
+        {
+            if (x * x + y * y == z * z || x * x + z * z == y * y || y * y + z * z == x * x)
+            {
+                Console.WriteLine("Unesene vrijednosti predstavljaju stranice pravokutnog trokuta.");
+            }
+            else
+            {
+                Console.WriteLine("Unesene vrijednosti ne predstavljaju stranice pravokutnog trokuta.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Unesene vrijednosti nisu smislene. Vrijednosti moraju biti veće od 0.");
+        }
+    }
